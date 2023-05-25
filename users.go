c57f84a6d891db47c9fbace008198cfc2deafa75
andres06-hub <andressierrarojas7@gmail.com>
2023-05-24 19:34:41 -0500

    build: init project

---
+++ users.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/andres06-hub/users/internal/config"
+	"github.com/andres06-hub/users/internal/server"
+	"github.com/andres06-hub/users/internal/svc"
+	"github.com/andres06-hub/users/users"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "etc/users.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		users.RegisterUsersServer(grpcServer, server.NewUsersServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
